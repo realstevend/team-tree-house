daaeb30e4b766d1d7de3afff8dcbc8c70b831666
realstevend <sdyson1@wgu.edu>
2018-06-28 14:51:45 -0400

    Allow for dynamic messaging using local storage

--- Projects/Java/course-ideas/src/main/java/com/teamtreehouse/courses/Main.java
+++ Projects/Java/course-ideas/src/main/java/com/teamtreehouse/courses/Main.java
@@ -3,12 +3,26 @@ package com.teamtreehouse.courses;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class Main {
     public static void main(String[] args) {
         get("/", (req, res) -> {
-            return new ModelAndView(null, "index.hbs");
+            Map<String, String> model = new HashMap<>();
+            model.put("username", req.cookie("username"));
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/sign-in", (req, res) -> {
+            Map<String, String> model = new HashMap<>();
+            String username = req.queryParams("username");
+            res.cookie("username", username);
+            model.put("username", username);
+            return new ModelAndView(model, "sign-in.hbs");
         }, new HandlebarsTemplateEngine());
     }
 }
